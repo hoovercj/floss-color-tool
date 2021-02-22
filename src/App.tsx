40bcf11ce6f2a603d8601fa0ae169c80d806d26c
Ace Young <ace.young289@gmail.com>
2021-02-21 22:23:20 -0500

    App.js: Update constructor to avoid build error

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import ColorTool from './Components/color-tool';
 
 const colors = require('./Resources/colorsDistanceMatrix.json');
 
-class App extends React.Component<{}, null> {
-
-  render() {
-    return (
-      <ColorTool colors={colors} />
-    );
-  }
+class App extends React.Component {
+    render() {
+        return <ColorTool colors={colors} />;
+    }
 }
 
 export default App;
