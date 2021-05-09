3a7e77facc37b0c029cf96f625b6b893365006f9
hoovercj <hoovercj@users.noreply.github.com>
2021-05-09 15:11:23 +0000

    deploy: d83c968d0c13232465a590a932b0fab57d559f17

---
+++ precache-manifest.d098fc98295e94661ea4d5f49a0f9320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e72d21c5134608e96ce643f56b46d9",
+    "url": "/floss-color-tool/index.html"
+  },
+  {
+    "revision": "67343d12520b0f95faa8",
+    "url": "/floss-color-tool/static/css/2.04196b2f.chunk.css"
+  },
+  {
+    "revision": "fb215029cd91fa70cf60",
+    "url": "/floss-color-tool/static/css/main.a7b5b9f0.chunk.css"
+  },
+  {
+    "revision": "67343d12520b0f95faa8",
+    "url": "/floss-color-tool/static/js/2.b7b346bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/floss-color-tool/static/js/2.b7b346bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb215029cd91fa70cf60",
+    "url": "/floss-color-tool/static/js/main.4ed20989.chunk.js"
+  },
+  {
+    "revision": "d61b1f07148cf5b6af31",
+    "url": "/floss-color-tool/static/js/runtime-main.78ab37cb.js"
+  }
+]);
