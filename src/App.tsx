c59b807f361d0c0a950e81225b0e10e090a25a47
borzechof99 <borzechof99@mi.fu-berlin.de>
2021-12-10 12:20:03 +0100

    Clean Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import { ColorTool } from './Components/color-tool';
-import { AllColors } from './Models/color';
+import CustomColorPicker from './Components/customColorPicker';
+import { AllColors, } from './Models/color';
 
 const allColors: AllColors = require('./Resources/colors.json');
 
@@ -36,11 +37,114 @@ const App = () => {
 
 const RoutedApp = () => {
     const { brand: brandParam } = useParams<{ brand?: string }>();
+
     const [filterText, setFilterText] = React.useState('');
+
+    // HTMLFilterText contains the content of the Text input and is updated on every InputChange
+    const [HTMLFilterText, setHTMLFilterText] = React.useState('');
+    // ActiveHTMLFilterText is the full hex code that gets used for sorting in the ColorTool. Only used when HTMLFilterText.length is 0 or 7
+    const [ActiveHTMLFilterText, setActiveHTMLFilterText] = React.useState('');
+
+    const [displaySketchPicker, setDisplay] = React.useState(false);
 
     const onInputChanged = React.useCallback((event: { target: { value: string } }) => {
         setFilterText(event.target.value);
     }, []);
+
+    const updateActiveHTMLInput = (text: string) => {
+        // Set a timeout to update the Filtertext to allow the Text in the input to update before the site freezes
+        setTimeout(
+            () => setActiveHTMLFilterText(text), 10
+        )
+    }
+
+    const onHTMLInputChanged = (event: { target: { value: string } }) => {
+        // Lots of String filtering to get a clean Hex Code
+
+        let new_text = event.target.value.toLowerCase().split('').filter(char => /[0-9a-f]/.test(char)).join("");
+
+        if (new_text.length === 0) {
+            setHTMLFilterText(new_text);
+            updateActiveHTMLInput(new_text);
+            return;
+        }
+
+        if (!new_text[0].startsWith("#")) {
+            new_text = "#" + new_text;
+        }
+
+        if (new_text.length > 7) {
+            new_text = new_text.substring(0, 7);
+        }
+
+        setHTMLFilterText(new_text);
+
+        if (new_text.length === 7) {
+            updateActiveHTMLInput(new_text);
+        }
+    };
+
+    const displayColorPicker = () => {
+
+        const colorPicker = () => {
+
+            if (displaySketchPicker) {
+
+                return (
+                    <div
+                        style={{ position: "absolute", marginTop: "2rem" }}
+                        onMouseLeave={() => setDisplay(false)}
+                    >
+                        <CustomColorPicker
+                            color={HTMLFilterText}
+                            onChange={(color: any) => onHTMLInputChanged({ target: { value: color.hex } })}
+                        />
+                    </div>
+                );
+            } else {
+                return null;
+            }
+        }
+
+        return (
+            <React.Fragment>
+
+                {/* Input for HexCodes */}
+                <input
+                    className="form-control"
+                    type="search"
+                    placeholder={"Search by HTML code"}
+                    aria-label={"Search by HTML code"}
+                    value={HTMLFilterText}
+                    onChange={onHTMLInputChanged}
+                >
+                </input>
+
+                {/* Color Preview Block */}
+                <div
+                    style={{
+                        display: "absolute",
+                        backgroundColor: HTMLFilterText,
+                        border: "1px solid #ced4da",
+                        borderRadius: "2px",
+                        width: "2rem",
+                        height: "2rem",
+                        marginLeft: "-2.2rem",
+                        marginTop: "auto",
+                        marginBottom: "auto",
+                        cursor: "pointer",
+                    }}
+                    onClick={() => setDisplay(true)}
+                >
+                    {/* Includes the Color Clicker which is displayed when clicked on */}
+                    {colorPicker()}
+                </div>
+
+
+            </React.Fragment>
+        )
+    }
+
 
     const brand = (brandParam && brands.find(brand => brand.toLowerCase() === brandParam.toLowerCase()))
         ?? brands[0];
@@ -59,6 +163,11 @@ const RoutedApp = () => {
                         ))}
                     </ul>
                 </div>
+
+                <form className="d-flex col-md-3" style={{ marginRight: "2rem" }}>
+                    {displayColorPicker()}
+                </form>
+
                 <form className="d-flex col-md-5">
                     <input
                         className="form-control"
@@ -72,14 +181,15 @@ const RoutedApp = () => {
                 </form>
             </div>
         </nav>
-        <div className='container-fl app-body'>
+        <div className='container-fluid app-body'> {/* Was container-fl once, typo? */}
             <div className='row'>
                 <div className='col-sm-10 offset-sm-1 col-xl-8 offset-xl-2'>
-                    <ColorTool brand={brand} colors={colors} filterText={filterText} />
+                    <ColorTool brand={brand} colors={colors} filterText={filterText} filterColor={ActiveHTMLFilterText} />
                 </div>
             </div>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default App;
